00b9667b7e6b5c6177b0f60fd4aa6f79bb150423
Hooiu <lycosloyal@naver.com>
2021-05-01 23:57:29 +0900

    Test

--- test.py
+++ test.py
@@ -4,17 +4,24 @@
 # print(gems)
 # print(gems['Ruby'][0])
 
-num = map(str, [1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5])
-left_fixed =['1','4','7']
-right_fixed = map(str,[3,6,9])
-main_hand = 'right'
-cur_left = '*'
-cur_right = '#'
-result_list = []
+# num = map(str, [1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5])
+# left_fixed =['1','4','7']
+# right_fixed = map(str,[3,6,9])
+# main_hand = 'right'
+# cur_left = '*'
+# cur_right = '#'
+# result_list = []
 
-for i in num:
-    if i in left_fixed:
-        result_list.append(cur_left)
-        cur_left = i
+# for i in num:
+#     if i in left_fixed:
+#         result_list.append(cur_left)
+#         cur_left = i
 
-print(result_list)
+# print(result_list)
+
+A = 2.0
+B = 2
+
+print(type(A), type(B))
+if A == B:
+    print("int와 float 비교 가능하네!")
