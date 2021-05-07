8f5551edc1b4ae57d88f8e57429dbb22ba1506a0
Hooiu <lycosloyal@naver.com>
2021-05-08 03:35:50 +0900

    #카카오 인턴십 2019, 문제 No.3, 제제 아이디 검토, (작성중)

--- test.py
+++ test.py
@@ -1,60 +1,51 @@
-# gems = {'Ruby':[3,2],'Dia':2}
-# print(len(gems))
-# # gems['Ruby'] += 1
-# print(gems)
-# print(gems['Ruby'][0])
+import itertools
 
-# num = map(str, [1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5])
-# left_fixed =['1','4','7']
-# right_fixed = map(str,[3,6,9])
-# main_hand = 'right'
-# cur_left = '*'
-# cur_right = '#'
-# result_list = []
+def solution(user_id, banned_id):
+    ban = dict()
+    cnt = 0
+    for b in banned_id :
+        real_str = b
+        cnt +=  1
+        # 중복되는 값을 방지하기 위해 뒤에 숫자를 붙인다.(중복되는 경우)
+        if b in ban :
+            b += str(cnt)
+        ban[b] = []
 
-# for i in num:
-#     if i in left_fixed:
-#         result_list.append(cur_left)
-#         cur_left = i
-
-# print(result_list)
-
-# A = 2.0
-# B = 2
-
-# print(type(A), type(B))
-# if A == B:
-#     print("int와 float 비교 가능하네!")
-
-
-# print([i for i in range(3)])
-
-
-def solution(v):
-    answer = []
-    x_list = []
-    y_list = []
-    x = 0
-    y = 0
-    for i in v:
-        x_list.append(i[0])
-        y_list.append(i[1])
-    if x[0] == x[1]:
-        x = x[2]
-    elif x[0] == x[2]:
-        x = x[1]
-    elif x[1] == x[2]:
-        x = x[0]
+        # 별의 우치를 찾는 작업
+        star = []
+        for s in range(len(real_str)) :
+            if real_str[s] == "*" : 
+                star.append(s)
+        
+        for u in user_id :
+            temp = 0
+            buf = ""
+            #  제재 아이디의 별의 위치에 맞도록 user id 를 수정 -> 비교 진행
+            for i in star :
+                if len(u) > i : 
+                    buf += u[temp : i] + "*"
+                    temp = i + 1
+            if len(u) != len(buf) :
+                buf += u[temp :]
+            if real_str == buf :
+                ban[b].append(u)
     
-    if y[0] == y[1]:
-        y = y[2]
-    elif y[0] == y[2]:
-        y = y[1]
-    elif y[1] == y[2]:
-        y = y[0]
-    
-    answer = [x,y]
+    ban_list = list(ban.values())
+    print(ban_list)
+    #  곱집합을 재재 리스트에 있는 아이디 들로 곱집합을 구하는 과정
+    #  -> 중복을 포함해서 모든 경우의 수를 구할 수 있다.
+    ans_list = list(itertools.product(*ban_list))
+    answer = set()
+    for a in ans_list:
+        # set 으로 중복을 제거했을때 항목 수가 줄어들었으면 pass 한다.
+        if len(set(a)) == len(banned_id):
+            #  정렬 후 문자열로 만들어 집어 넣어 중목을 방지한다.
+            #  같은 것이 두개 들어가도 set로 설정되어 있기 때문에 중복되지 않는다.
+            answer.add("".join(sorted(set(a))))
+    print(answer)
+    return len(answer)
 
-    return answer
+user_id_list = ["frodo", "fradi", "crodo", "abc123", "frodoc"]
+banned_id_list = ["fr*d*", "*rodo", "******", "******"]
 
-print(solution([[1, 4], [3, 4], [3, 10]]))
+print(solution(user_id_list, banned_id_list))
