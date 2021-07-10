0eb2032dfeeebccecfefba4258a3eb80f02ae5aa
Hooiu <lycosloyal@naver.com>
2021-07-11 00:40:12 +0900

    2021카카오 인턴십 1번문제

--- Internship2021_No1.py
+++ Internship2021_No1.py
@@ -6,17 +6,17 @@
 # replace를 이용하여 for문 작성
 
 
-text = "one4seveneight"
-# 0	zero
-# 1	one
-# 2	two
-# 3	three
-# 4	four
-# 5	five
-# 6	six
-# 7	seven
-# 8	eight
-# 9	nine
+# text = "one4seveneight"
+# 0 zero
+# 1 one
+# 2 two
+# 3 three
+# 4 four
+# 5 five
+# 6 six
+# 7 seven
+# 8 eight
+# 9 nine
 
 
 # 어떻게 문자를 나눌까???
@@ -27,23 +27,21 @@ text = "one4seveneight"
 #  2. 숫자가 아닐 경우
 #      temp_text += text_value #계속 문자를 더하고
 #      if temp_text in numbers # 딕셔너리에 있으면 저장
-#  
+def solution(text):
 
-numbers_alpha = {'zero':'0','one':'1','two':'2','three':'3','four':'4','five':'5','six':'6','seven':'7','eight':'8','nine':'9'}
-numbers_arabia = ['0','1','2','3','4','5','6','7','8','9']
+    numbers_alpha = {'zero':'0','one':'1','two':'2','three':'3','four':'4','five':'5','six':'6','seven':'7','eight':'8','nine':'9'}
+    numbers_arabia = ['0','1','2','3','4','5','6','7','8','9']
 
-result_list = []
-temp_text = ''
-for text_value in text:
-    if text_value in numbers_arabia:
-        result_list.append(text_value) #숫자일 경우
-    else:                               #숫자가 아닐경우
-        temp_text += text_value        #한글자씩 문자를 더하고
-        if temp_text in numbers_alpha:
-            result_list.append(numbers_alpha[temp_text])
-            temp_text = ''
+    result_list = []
+    temp_text = ''
+    for text_value in text:
+        if text_value in numbers_arabia:
+            result_list.append(text_value) #숫자일 경우
+        else:                               #숫자가 아닐경우
+            temp_text += text_value        #한글자씩 문자를 더하고
+            if temp_text in numbers_alpha:
+                result_list.append(numbers_alpha[temp_text])
+                temp_text = ''
 
-print(result_list)
-result = ''.join(result_list)
-print(result)
-
+    result = int(''.join(result_list))
+    return result
